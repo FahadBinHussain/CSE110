075308fd379e2d26488e044a066717d6961310ed
Fahad Bin Hussain <fahadbinhussain@outlook.com>
2023-02-16 14:38:23 +0000

    finish 1-19

--- a3/16.py
+++ a3/16.py
@@ -1,13 +1,13 @@
 num = int(input("Enter the quantity of numbers: "))
-max_num = None
-min_num = None
+max_num = 0
+min_num = 0
 sum = 0
 
 for i in range(num):
     x = int(input("Enter a number: "))
-    if max_num is None or x > max_num:
+    if max_num == 0 or x > max_num:
         max_num = x
-    if min_num is None or x < min_num:
+    if min_num == 0 or x < min_num:
         min_num = x
     sum += x
 
