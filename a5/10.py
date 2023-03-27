5ba9cd9da1d31b7fc1b0e28a6e099a3ca0279376
Fahad Bin Hussain <fahadbinhussain@outlook.com>
2023-03-27 06:16:22 +0000

    add 10

--- a5/10.py
+++ a5/10.py
@@ -2,12 +2,19 @@ user_input = input()
 user_input += ", "
 num = ""
 input_list = []
+modified_list = []
 
 for chr in user_input:
-  if chr != "," and chr != " ":
-    num += chr
+  if chr != ",":
+    if chr != " ":
+      num += chr
   else:
     input_list.append(int(num))
-    num = ""
+    if int(num) not in modified_list:
+      modified_list.append(int(num))
+      num = ""
+    else:
+      num = ""
       
-print(input_list)
+print(f"Input list: {input_list}")
+print(f"Modified list: {modified_list}")
