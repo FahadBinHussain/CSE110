a64a74cafce05fe86b485ebe5b54f2b6e0dd9864
Fahad Bin Hussain <fahadbinhussain@outlook.com>
2023-03-29 06:01:03 +0000

    Correct 6

--- a6/6.py
+++ a6/6.py
@@ -1,6 +1,9 @@
 tuple1 = ('a', 'b', 'c', 'd', 'e', 'f', 'g', 'h')
+list1 = []
 
-list1 = list(tuple1)
+for element in tuple1:
+    list1.append(element)
+
 final_list = []
 
 for i in range(len(list1)-1, -1, -1):
