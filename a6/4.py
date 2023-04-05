eeb5d8ae6657865989efdbbc22b3e9e4916f04ed
Fahad Bin Hussain <fahadbinhussain@outlook.com>
2023-04-05 04:40:04 +0000

    Beautify a6/4.py and 5.py

--- a6/4.py
+++ a6/4.py
@@ -1,7 +1,7 @@
 book_info = (
 ("Best Mystery & Thriller","The Silent Patient",68821),
 ("Best Horror","The Institute",75717),
-("Best History & Biography","The five",31783 ),
+("Best History & Biography","The five",31783),
 ("Best Fiction","The Testaments",98291)
 )
 
